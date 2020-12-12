717d76c819751e51d46ca67bb136d302e151a00e
Lee Dong Hee <jjaing01@naver.com>
2020-12-12 17:29:28 +0900

    Struct Main Server Base (2020.12.12)

---
+++ IOCPServer_LDH_2015182030/IOCPServer_LDH_2015182030/stdafx.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* SYSTEM HEADER */
+#include <iostream>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#include <thread>
+#include <mutex>
+#include <unordered_set>
+#include <algorithm>
+#include <chrono>
+#include <queue>
+#include <atomic>
+
+/* USER HEADER */
+#include "protocol.h"
+
+/* NAMESPACE */
+using namespace std;
+using namespace chrono;
+
+/* LINK TO LIBRARY */
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+#pragma comment(lib, "lua54.lib")
